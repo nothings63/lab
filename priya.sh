1d1fffb4638cfee40c78ae1536942be842b528d2
nothings63 <nothings1q@gmail.com>
2024-08-06 10:34:50 +0530

    Delete priya.sh

--- priya.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-read -p "Enter a string: " str
-
-if [ "$(echo $str | rev)" == "$str" ]; then
-    echo "The string '$str' is a palindrome."
-else
-    echo "The string '$str' is not a palindrome."
-fi
